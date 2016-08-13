c2269c4d5d7530aff51e0403172cf4a72efb7260
GabyFraga <gfmmedeiros@sga.pucminas.br>
2016-08-13 12:39:25 -0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t = 0, a, b;
+	
+	scanf("%d", &t);
+	
+	while(t > 0){
+	
+		scanf("%d %d", &a, &b);
+		
+		if(a > b){
+			
+			cout << ">\n";
+			
+		}else if(a == b){
+			
+			cout << "=\n";
+			
+		}else{
+			
+			cout << "<\n";
+			
+		}
+		
+		t--;
+		
+	}
+	
+	return 0;	
+	
+}
